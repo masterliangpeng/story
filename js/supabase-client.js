2a4e51c5cfb9562929b1083bbbe7c59aec594fc5
masterliangpeng <53523504+masterliangpeng@users.noreply.github.com>
2025-06-03 23:04:37 +0800

    update
    update

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -3,8 +3,6 @@ const supabaseUrl = 'https://cxtvmolpayeplvdxcjvf.supabase.co';
 const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dHZtb2xwYXllcGx2ZHhjanZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NTE0NTgsImV4cCI6MjA2MTEyNzQ1OH0.LIBg-OUlZHE6jtct1hRBFn1uOw6upqyDOCK-qBqaXic';
 
 
-console.log('正在初始化Supabase客户端...');
-
 // 从全局supabase对象中解构出createClient方法
 let supabaseClient;
 try {
