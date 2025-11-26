e8318697a20c3b5cf94ffa8914482f7df97aed70
masterliangpeng <2944875019@qq.com>
2025-11-26 09:33:55 +0800

    Update supabase-client.js
    update

--- js/supabase-client.js
+++ js/supabase-client.js
@@ -1,6 +1,6 @@
-// 初始化Supabase客户端
-const supabaseUrl = 'https://cxtvmolpayeplvdxcjvf.supabase.co';
-const supabaseKey = 'eyJhbGciOiJIUzI1NiIsInR5cCI6IkpXVCJ9.eyJpc3MiOiJzdXBhYmFzZSIsInJlZiI6ImN4dHZtb2xwYXllcGx2ZHhjanZmIiwicm9sZSI6ImFub24iLCJpYXQiOjE3NDU1NTE0NTgsImV4cCI6MjA2MTEyNzQ1OH0.LIBg-OUlZHE6jtct1hRBFn1uOw6upqyDOCK-qBqaXic';
+// 初始化Supabase客户端 supabaseUrl和supabaseKey都是从supabase服务器中获取
+const supabaseUrl = '';
+const supabaseKey = '';
 
 
 // 从全局supabase对象中解构出createClient方法
